0629427c5ef85c57c533d2d9d77842efdc80681d
Julien NOEL <julien.leon99@gmail.com>
2017-12-27 20:57:13 +0100

    create a window

--- TheGroMAZE.py
+++ TheGroMAZE.py
@@ -1,1 +1,16 @@
+from upemtk import *
+
+
 print ("Hello World")
+
+cree_fenetre(800,600)
+
+while True:
+
+	mise_a_jour()
+	ev = donne_evenement()
+	ty = type_evenement(ev)
+	if ty == 'Quitte':
+		break
+
+ferme_fenetre()
