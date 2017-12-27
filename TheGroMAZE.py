85adf68bdf92538e7025f80e2e41c0c87c89ec9d
Julien NOEL <julien.leon99@gmail.com>
2017-12-27 21:58:24 +0100

    add function bouger

--- TheGroMAZE.py
+++ TheGroMAZE.py
@@ -1,16 +1,43 @@
 from upemtk import *
+from time import sleep
 
 
 print ("Hello World")
 
+labyrinthe=[
+	["*","*","*","*"],
+	["*","@","*","*"],
+	["*",".",".","*"],
+	["*","*","X","*"]
+]
+
+def bouger():
+	for i in range(len(labyrinthe)):
+		x = labyrinthe[i]
+		for a in range(len(x)):
+			if x[a] == "@":
+				x[a] = "."
+				labyrinthe[i+1][a] = "@"
+				return
+
+
 cree_fenetre(800,600)
 
-while True:
+framerate = 5
 
+while True:
+	print(labyrinthe)
 	mise_a_jour()
 	ev = donne_evenement()
 	ty = type_evenement(ev)
 	if ty == 'Quitte':
 		break
+	elif ty == "Touche":
+		print(touche(ev))
+		if touche(ev) == "Down":
+			bouger()
+		elif touche(ev) == "Escape":
+			break
+	sleep(1 / framerate)
 
 ferme_fenetre()
